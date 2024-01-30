7d0ee1ebd54aace6caac3bd3c187fd3b33eedb4b
Sujeet <singhsujeet772@gmail.com>
2024-01-30 12:23:03 +0530

    Load controller

--- Application/Controller/UserController.php
+++ Application/Controller/UserController.php
@@ -4,4 +4,9 @@ class UserController{
     public function index(){
         echo "UserController";
     }
+
+    public function userMethod($num1 ,$num2){
+        echo "My name is sujeet kumar singh<br>";
+        echo $num1." ".$num2;
+    }
 }
--- System/Classes/Router.php
+++ System/Classes/Router.php
@@ -11,11 +11,35 @@ class Router{
         $url = $this->url();
         if(!empty($url)){
             if(file_exists("../Application/Controller/". $url[0] .".php")){
-                echo "Controller Found";
+                $this->controller = $url[0];
+                unset($url[0]);
             }else{
-                echo "Controller Not Found";
+                echo "<div style=margin:0;padding:10px;background-color:silver>$url[0].php Not Found</div>";
+            }
+
+            //Include Controller
+            require_once "../Application/Controller/" . $this->controller . ".php";
+
+            //instantiate controller
+            $this->controller = new $this->controller;
+            
+            if(isset($url[1])){
+                if(method_exists($this->controller, $url[1])){
+                    $this->method = $url[1];
+                    unset($url[1]);
+                }else{
+                    echo "<div style=margin:0;padding:10px;background-color:silver>$url[1] method Not Found</div>";
+                    // die('Method '.$url[1].' not found in '. $this->controller.'.php');  
+                }
             }
         }
+        if(isset($url)){
+            $this->params = $url;
+        }else{
+            $this->params = [];
+        }
+
+        call_user_func_array([$this->controller , $this->method] , $this->params);
     }
 
     public function url(){
